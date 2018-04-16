ee0a8b621eab95adb4b0e146cfe98bf01d081598
davisv007 <davisvberea@gmail.com>
2018-04-15 20:08:54 -0400

    brought the right main back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import ast
-from sudoclasses import Population
-
-
+from popclass import Population
+import time
 
 def find_values(board):
     indices = {}
@@ -19,11 +18,23 @@ def import_problem():
         return board
 
 
+def import_solution():
+    with open('solution.in', 'r') as fileobj:
+        lines = fileobj.readlines()
+        board = [ast.literal_eval(x) for x in lines]
+        return board
+
+
 def main():
+    start = time.time()
     board = import_problem()
     given_values = find_values(board)
-    pop = Population(15000, 5000,1, 1, given_values,100)
-    print(pop.solution)
+    pop = Population(1000, 50000, 2, 0.01, given_values, 0)
+    [print(x) for x in pop.solution]
+    print(import_solution() == pop.solution)
+    end = time.time()
+    print('Time taken: {}'.format((end-start)/60))
 
 
 main()
+
